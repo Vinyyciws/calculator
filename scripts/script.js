a66a23aed64fd4f1d674d8976107294f248f2f84
Vinícius Conde Vasconcelos <136513095+Vinyyciws@users.noreply.github.com>
2024-10-16 17:22:55 -0300

    Alterarions on Script

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-function screenRes(iRes) {
-    const number = 1
-
+let number = Number(document.querySelector('button.value'));
+function screenRes() {
+    const visor = document.querySelector('input#iRes')
+    return number
 }
