d360ef05b94b57eb7bd16647be30a86649862344
Vinícius Conde Vasconcelos <136513095+Vinyyciws@users.noreply.github.com>
2024-10-17 16:41:10 -0300

    Adding variable values through buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
-let number = Number(document.querySelector('button.value'));
-function screenRes() {
-    const visor = document.querySelector('input#iRes')
-    return number
+const visor = document.querySelector('input#iRes');
+let num = document.querySelector('button.value');
+
+function screenRes(n) {
+    num = Number(n)
+    console.log(num)
 }
